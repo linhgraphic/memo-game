82ed4b9a0623df66e5ec6b18e7771ed4627d031e
Linh <linhgraphic@gmail.com>
2021-01-30 00:25:31 +0000

    Updates

---
+++ precache-manifest.f7b9178745fdc833fdb63a9ff6552e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ef8769a795c6561e3ea967f5fd9c30",
+    "url": "/memo-game/index.html"
+  },
+  {
+    "revision": "8ec27f1e11f1f00dc1ff",
+    "url": "/memo-game/static/css/main.8cb7a27f.chunk.css"
+  },
+  {
+    "revision": "3ee5f0b209d44dd82526",
+    "url": "/memo-game/static/js/2.354405fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memo-game/static/js/2.354405fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec27f1e11f1f00dc1ff",
+    "url": "/memo-game/static/js/main.a1e99586.chunk.js"
+  },
+  {
+    "revision": "c356bfc4632929742fe6",
+    "url": "/memo-game/static/js/runtime-main.a3d1f000.js"
+  }
+]);
